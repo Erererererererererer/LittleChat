468b951a5b5fc00bc5fda39df31867375531395a
cc <1365790348@qq.com>
2024-11-26 13:02:24 +0800

    新增实体类

--- src/main/java/com/bitcser/littlechat/mapper/UserMapper.java
+++ src/main/java/com/bitcser/littlechat/mapper/UserMapper.java
@@ -1,19 +1,23 @@
 package com.bitcser.littlechat.mapper;
 
 import com.bitcser.littlechat.entity.User;
-import org.apache.ibatis.annotations.Delete;
 
 import java.util.List;
 
 public interface UserMapper {
 
+    // 增
     void insert(User user);
 
+    // 根据ID删
     void deleteById(Integer id);
 
+    // 根据ID改
     void updateById(User user);
 
+    // 查所有
     List<User> selectAll();
 
+    // 根据ID查
     User selectById(Integer id);
 }
