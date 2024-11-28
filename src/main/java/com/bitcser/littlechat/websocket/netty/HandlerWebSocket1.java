cd69cdb3e0ce58511a3e06cecddc38e34f8f5797
cc <1365790348@qq.com>
2024-11-28 13:31:32 +0800

    更改websocket，使聊天用socket通信，其余时候用http请求

--- src/main/java/com/bitcser/littlechat/websocket/netty/HandlerWebSocket1.java
+++ src/main/java/com/bitcser/littlechat/websocket/netty/HandlerWebSocket1.java
@@ -1,16 +1,17 @@
 package com.bitcser.littlechat.websocket.netty;
+
 import com.bitcser.littlechat.common.Receive;
 import com.bitcser.littlechat.common.Result;
 import com.bitcser.littlechat.service.ChatRecordSevice;
 import com.bitcser.littlechat.service.MessageService;
-import com.bitcser.littlechat.websocket.ChannelContextUtils;
 import com.bitcser.littlechat.service.UserService;
-
+import com.bitcser.littlechat.websocket.ChannelContextUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.netty.channel.*;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
-import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
-import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
 import jakarta.annotation.Resource;
@@ -18,13 +19,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import javax.imageio.stream.ImageOutputStream;
-import java.util.Arrays;
-
-// handler（新版，对应逻辑为无条件建立socket连接，登录一起放入后续操作；并且聊天用socket通信，其余时候用http请求）
+// handler（半新版，对应逻辑为无条件建立socket连接，登录一起放入后续操作；并且全程只用socket通信，而不使用http）
 @Component
 @ChannelHandler.Sharable
-public class HandlerWebSocket extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+public class HandlerWebSocket1 extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
     @Resource
     private ChannelContextUtils channelContextUtils;
@@ -41,7 +39,7 @@ public class HandlerWebSocket extends SimpleChannelInboundHandler<TextWebSocketF
     @Resource
     private ChatRecordSevice chatRecordSevice;
 
-    private static final Logger logger = LoggerFactory.getLogger(HandlerWebSocket.class);
+    private static final Logger logger = LoggerFactory.getLogger(HandlerWebSocket1.class);
 
     //  通道就绪后调用，一般用来做初始化
     @Override
@@ -66,34 +64,39 @@ public class HandlerWebSocket extends SimpleChannelInboundHandler<TextWebSocketF
         String text = textWebSocketFrame.text(); // 消息内容
         logger.info("收到消息：{}",  text);
 
-        if (!text.contains("/")) {
-            // 无法解析，格式错误
-            channelContextUtils.sendMessage(channel, "error");
-            return;
-        }
-
-        String method = text.split("/")[0];
-        if (method.equals("login")) {
-            String userId = text.split("/")[1];
+        String userId = null;
+        receive.splitText(text);
+        if (receive.getModule().equals("user") && receive.getService().equals("login")) {
+            userId = receive.getParamsMap().get("id");
             // 绑定channel和用户ID
             channelContextUtils.addContext(userId, ctx.channel());
-        } else if (method.equals("chat")) {
-            String receiverId = text.split("/")[1];
-            String message = text.split("/")[2];
-
+        } else {
             // 获取用户ID
             Attribute<String> attribute = channel.attr(AttributeKey.valueOf(channel.id().toString()));
-            String userId = attribute.get();
+            userId = attribute.get();
+        }
 
-            // 发送聊天消息（如果用户在线，则直接发送；不在线则存数据库）
+        // 调用Service进行处理
+        Result result = receive.runService();
+        result.setSenderId(userId);
+
+        // 发送反馈消息
+        // 将Result对象序列化为JSON字符串
+        ObjectMapper mapper = new ObjectMapper();
+        String message = mapper.writeValueAsString(result);
+        channelContextUtils.sendMessage(channel, message);
+        // channelContextUtils.sendMessage(userId, message);
+        // 发送聊天消息（如果用户在线，则直接发送；不在线则存数据库）
+        String receiverId = result.getReceiverId();
+        if (receiverId != null) {
             if (userService.online(Integer.valueOf(receiverId))) {
                 // 发送，存message
-                channelContextUtils.sendMessage(receiverId, "chat/" + userId + "/" + message);
-                messageService.add(Integer.valueOf(userId), Integer.valueOf(receiverId), message);
+                channelContextUtils.sendMessage(receiverId, message);
+                messageService.add(Integer.valueOf(userId), Integer.valueOf(receiverId), (String)result.getData());
             } else {
                 // 存message，更新record
-                messageService.add(Integer.valueOf(userId), Integer.valueOf(receiverId), message);
-                chatRecordSevice.update(Integer.valueOf(userId), Integer.valueOf(receiverId), message);
+                messageService.add(Integer.valueOf(userId), Integer.valueOf(receiverId), (String)result.getData());
+                chatRecordSevice.update(Integer.valueOf(userId), Integer.valueOf(receiverId), (String)result.getData());
             }
         }
     }
