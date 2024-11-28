7f9c41c03ca3a2e5c26103a31bc37b4fd4e02ece
cc <1365790348@qq.com>
2024-11-28 15:33:49 +0800

    friend在线列表，多表联查&排序

--- src/main/java/com/bitcser/littlechat/controller/FriendController.java
+++ src/main/java/com/bitcser/littlechat/controller/FriendController.java
@@ -3,10 +3,11 @@ package com.bitcser.littlechat.controller;
 import com.bitcser.littlechat.common.Result;
 import com.bitcser.littlechat.entity.Friend;
 import com.bitcser.littlechat.service.FriendService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/friend")
@@ -33,8 +34,37 @@ public class FriendController {
     }
 
     @GetMapping("/findAll")
-    public Result findAll(@RequestParam Integer userId) {
-        List<Friend> friendList = friendService.findAll(userId);
-        return Result.success(friendList);
+    public Result findAll(@RequestParam Integer id) {
+        List<Friend> friendList = friendService.findAll(id);
+        List<Map<String, Object>> friendInfoList = new ArrayList<>();
+        for (Friend friend : friendList) {
+            Map<String, Object> friendInfo = new HashMap<>();
+            friendInfo.put("status", friend.getStatus());
+            friendInfo.put("id", friend.getUser().getId());
+            friendInfo.put("username", friend.getUser().getUsername());
+            friendInfo.put("avatar", friend.getUser().getAvatar());
+            friendInfo.put("email", friend.getUser().getEmail());
+            friendInfo.put("phone", friend.getUser().getPhone());
+            friendInfo.put("gender", friend.getUser().getGender());
+            friendInfo.put("online", friend.getUser().getOnline());
+            friendInfoList.add(friendInfo);
+        }
+
+        // 排序（在线为第一优先，用户名为第二优先）
+        Collections.sort(friendInfoList, new Comparator<Map<String, Object>>() {
+            public int compare(Map<String, Object> o1, Map<String, Object> o2) {
+                String one = String.valueOf(o1.get("username").toString());
+                String two = String.valueOf(o2.get("username").toString());
+                return one.compareTo(two);
+            }
+        });
+        Collections.sort(friendInfoList, new Comparator<Map<String, Object>>() {
+            public int compare(Map<String, Object> o1, Map<String, Object> o2) {
+                Integer one = Integer.valueOf(o1.get("online").toString());
+                Integer two = Integer.valueOf(o2.get("online").toString());
+                return -one.compareTo(two);
+            }
+        });
+        return Result.success(friendInfoList);
     }
 }
--- src/main/java/com/bitcser/littlechat/entity/Friend.java
+++ src/main/java/com/bitcser/littlechat/entity/Friend.java
@@ -1,10 +1,13 @@
 package com.bitcser.littlechat.entity;
 
+import java.util.List;
+
 public class Friend {
     private Integer id;
     private Integer userId;;
     private Integer friendId;
     private Integer status;
+    private User user; // 用于联合查询
 
     public Integer getId() {
         return id;
@@ -36,5 +39,13 @@ public class Friend {
 
     public void setStatus(Integer status) {
         this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
